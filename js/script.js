91fad589d43c57a2bfaaf390095439bc8052026a
Guillermo-31 <guillermogr211@gmail.com>
2022-10-19 13:17:41 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+function initializar(){
+  document.getElementById("guardar").addEventListener("click", function(){
+    guardar();
+  });
+}
+
 function guardar(){
   let nombre = document.getElementById('nombre').value;
   let dui = document.getElementById('dui').value;
@@ -6,22 +12,63 @@ function guardar(){
   let modelo = document.getElementById('modelo').value;
   let year = document.getElementById('year').value;
   let color = document.getElementById('color').value;
+  let placa = document.getElementById('placa').value;
   let fallas = document.getElementById('fallas').value;
 
-  if (!validar(nombre, dui, nit, year, fallas)){
-    return false;
-  }
+  /*if (!validar(nombre, dui, nit, year, placa, fallas)){
+    return 0;
+  }*/
+
+  let info = [
+    nombre, nit, marca, modelo, year, color, placa, fallas
+  ];
+
+  localStorage.setItem(dui, JSON.stringify(info));
+  mostrar();
 }
 
-function validar(nombre, dui, nit, year, fallas){
+function validar(nombre, dui, nit, year, placa, fallas){
   let expNom = /^[a-zA-Z\u00C0-\u017F\s]+$/;
-  let expDui = /^[a-zA-Z\u00C0-\u017F\s]+$/;
-  let expFallas = /^[a-zA-Z\u00C0-\u017F\s]+$/;
-  let placa = /^[a-zA-Z\u00C0-\u017F\s]+$/;
-  let nit = /^\d{4}-\d{6}-\d{3}-\d$/; 
+  let expDui = /^\d{8}-\d$/;
+  let expFallas = /^([\w\u00C0-\u017F]+\s*[0-9]*)*.*\w*$/m;
+  let expPlaca = /^[A-Za-z0-9]/;
+  let expNit = /^\d[0-9]/
 
   if (!expNom.test(nombre) || nombre == ""){
     alert('ERROR: El nombre ingresado es inválido');
-    return false;
+    return 0;
+  }
+
+  if (localStorage.length === 0){
+    console.log("No hay nada");
+    return 0;
+  }else{
+    console.log("hay algo");
+    for (let i = 0; i < localStorage.length; i++){
+      if (localStorage.key(i) == dui){
+        
+      }
+    }
   }
 }
+
+function mostrar(){
+  let tabla = document.getElementById('datos');
+    tabla.innerHTML = "<table id='tablaResul'><tr class='filaTit'><td class='titulo'>Nombre</td><td class='titulo'>DUI</td><td class='titulo'>NIT</td><td class='titulo'>Marca</td><td class='titulo'>Modelo</td><td class='titulo'>Año</td><td class='titulo'>Color</td><td class='titulo'>Placa</td><td class='titulo'>Fallas</td></tr>";
+    tabla = document.getElementById('tablaResul');
+  if (localStorage.length === 0){
+    return 0;
+  }else{
+    for (let i = 0; i < localStorage.length; i++){
+      const key = localStorage.key(i);
+      let tdDui = localStorage.key(i).toString();
+      console.log(tdNom);
+      let info = localStorage.setItem(key, JSON.stringify(info));
+      console.log(tdProm);
+      tabla.innerHTML += "<tr><td class='cuerpo'>"+info[0]+"</td><td class='cuerpo'>"+tdDui+"</td><td class='cuerpo'>"+info[1]+"</td><td class='cuerpo'>"+info[2]+"</td><td class='cuerpo'>"+info[3]+"</td><td class='cuerpo'>"+info[4]+"</td><td class='cuerpo'>"+info[5]+"</td><td class='cuerpo'>"+info[6]+"</td><td class='cuerpo'>"+info[7]+"</td></tr>";
+    }
+    tabla.innerHTML += "</table>";
+  }
+}
+
+window.onload = initializar;

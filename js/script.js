1b9c18867ffce97c7d012c7db95e20c12de1f93d
Jefferson-Castro23 <73444981+Jefferson-Castro23@users.noreply.github.com>
2022-10-19 11:42:56 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,6 +17,8 @@ function validar(nombre, dui, nit, year, fallas){
   let expNom = /^[a-zA-Z\u00C0-\u017F\s]+$/;
   let expDui = /^\d{8}-\d$/;
   let expFallas = /^([\w\u00C0-\u017F]+\s*[0-9]*)*.*\w*$/m;
+  let placa = /^[A-Za-z0-9];
+  let nit = /^\d[0-9]
 
   if (!expNom.test(nombre) || nombre == ""){
     alert('ERROR: El nombre ingresado es inválido');

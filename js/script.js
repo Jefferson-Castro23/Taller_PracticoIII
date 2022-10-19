3abe6c1955d4e23eb0dc7b21640c8dd5f2a6f57f
Guillermo-31 <guillermogr211@gmail.com>
2022-10-19 11:22:39 -0600

    index arreglado

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function guardar(){
+  let nombre = document.getElementById('nombre').value;
+  let dui = document.getElementById('dui').value;
+  let nit = document.getElementById('nit').value;
+  let marca = document.getElementById('marca').value;
+  let modelo = document.getElementById('modelo').value;
+  let year = document.getElementById('year').value;
+  let color = document.getElementById('color').value;
+  let fallas = document.getElementById('fallas').value;
+
+  validar(nombre, dui, nit, marca, modelo, year, color, fallas);
+}
+
+function validar(nombre, dui, nit, marca, modelo, year, color, fallas){
+  let 
+}

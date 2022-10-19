baa64afd79b858b4cac804b1b181bcc378c1f4ee
Jefferson-Castro23 <73444981+Jefferson-Castro23@users.noreply.github.com>
2022-10-19 12:58:13 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-function initializar(){
-  document.getElementById("guardar").addEventListener("click", function(){
-    guardar();
-  });
-}
-
 function guardar(){
   let nombre = document.getElementById('nombre').value;
   let dui = document.getElementById('dui').value;
@@ -12,70 +6,23 @@ function guardar(){
   let modelo = document.getElementById('modelo').value;
   let year = document.getElementById('year').value;
   let color = document.getElementById('color').value;
-  let placa = document.getElementById('placa').value;
   let fallas = document.getElementById('fallas').value;
 
-  /*if (!validar(nombre, dui, nit, year, placa, fallas)){
+  if (!validar(nombre, dui, nit, year, fallas)){
     return 0;
-  }*/
-
-  let info = [
-    nombre, dui, nit, marca, modelo, year, color, placa, fallas
-  ];
-
-  localStorage.setItem(dui, JSON.stringify(info));
-  mostrar();
+  }
 }
 
-function validar(nombre, dui, nit, year, placa, fallas){
+function validar(nombre, dui, nit, year, fallas){
   let expNom = /^[a-zA-Z\u00C0-\u017F\s]+$/;
-  let expDui = /^\d{8}-\d$/;
+  let expDui = /^[a-zA-Z\u00C0-\u017F\s]+$/;
   let expFallas = /^([\w\u00C0-\u017F]+\s*[0-9]*)*.*\w*$/m;
-  let expPlaca = /^[A-Za-z0-9]/;
-  let expNit = /^\d[0-9]/
+  let placa = /^[a-zA-Z\u00C0-\u017F\s]+$/;
+  let nit = /^\d{4}-\d{6}-\d{3}-\d$/;
+  let 
 
   if (!expNom.test(nombre) || nombre == ""){
     alert('ERROR: El nombre ingresado es inválido');
     return 0;
   }
-
-  if (localStorage.length === 0){
-    console.log("No hay nada");
-    return 0;
-  }else{
-    console.log("hay algo");
-    for (let i = 0; i < localStorage.length; i++){
-      if (localStorage.key(i) == dui){
-        
-      }
-    }
-  }
 }
-
-function mostrar(dui){
-  let info = localStorage.getItem('dui');
-  let tabla = document.getElementById('datos');
-    tabla.innerHTML = "<table id='tablaResul'><tr class='filaTit'><td class='titulo'>Estudiante</td><td class='titulo'>Promedio</td></tr>";
-    tabla = document.getElementById('tablaResul');
-  if (localStorage.length === 0){
-    console.log("No hay nada");
-    return 0;
-  }else{
-    console.log("hay algo");
-    for (let i = 0; i < localStorage.length; i++){
-      console.log(localStorage.key(i).toString());
-      const key = localStorage.key(i);
-      let tdNom = localStorage.key(i).toString();
-      console.log(tdNom);
-      let tdProm = localStorage.getItem(key).toString();
-      console.log(tdProm);
-      if (i%2 == 0)
-        tabla.innerHTML += "<tr class='filaCuerpo-Impar'><td class='cuerpo'>"+tdNom+"</td><td class='cuerpo'>"+tdProm+"</td></tr>";
-      else
-        tabla.innerHTML += "<tr class='filaCuerpo-Par'><td class='cuerpo'>"+tdNom+"</td><td class='cuerpo'>"+tdProm+"</td></tr>";
-    }
-    tabla.innerHTML += "</table>";
-  }
-}
-
-window.onload = initializar;
